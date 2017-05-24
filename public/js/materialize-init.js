2db193993e45f9075c40ef0e3ff0dbfc427937ea
Andela Developer <andeladeveloper@Esther-Falayi.local>
2017-05-24 13:59:50 +0100

    feat(145026979): landing page - display app logoin header when page is scrolled - add slider to How-to-play section [Delivers #145026979]

--- public/js/materialize-init.js
+++ public/js/materialize-init.js
@@ -1,8 +1,32 @@
-(function($){
+$(document).ready(function(){
   $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+        // $('.carousel').carousel();
+        $('.carousel.carousel-slider').carousel({fullWidth: true});
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.modal').modal();
+        
+        var page = $('#page'); 
+        var mainHeader = $('.main-header');
+        var logo = $('.main-header .brand-logo a:not(.btn)');
+        page.on('scroll', function(event){
+           if (this.scrollTop >= 430) {
+            mainHeader.addClass("fix-header");
+            logo.removeClass("hide");
+          } else {
+            mainHeader.removeClass("fix-header"); 
+            logo.addClass("hide");                      
+          }
+        });
+        $('#how-to-play a:not(.carousel-item)')
+        .each(function(index){
+          $(this).click(function(event){
+            event.preventDefault();
+            $('.carousel').carousel('set', index + 1);
+          });
+        });
+        $('.scrollspy').scrollSpy({
+          scrollOffset: 100
+        });
+      }); 
+});
